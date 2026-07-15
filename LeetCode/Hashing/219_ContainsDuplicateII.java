f389b3e4e990ba03b5c60933b198dba97674225c
Indresh Pratap Singh <indreshpratapsinghh@gmail.com>
2026-07-15 17:49:11 +0530

    Added LeetCode 219 Conatins Duplicate II

---
+++ LeetCode/Hashing/219_ContainsDuplicateII.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> lastSeen= new HashMap<>();
+
+        for(int i=0; i<nums.length;i++){
+
+            if(lastSeen.containsKey(nums[i])){
+                if(Math.abs(i-lastSeen.get(nums[i]))<=k){
+                    return true;
+                }
+            }
+            lastSeen.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
